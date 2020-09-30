7611ec8285417d90d78db73fb3d4b4eefacc56c6
nickg <nickg@email>
2020-09-30 12:12:59 +0300

    Changed the form-demo to use FormGroup

--- src/app/components/form-demo/form-demo.component.ts
+++ src/app/components/form-demo/form-demo.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form-demo',
@@ -11,15 +11,25 @@ export class FormDemoComponent {
   options = ['one', 'two', 'three', 'four', 'five', 'six'];
 
   simpleDropdownFormControl = new FormControl('');
-  featuredDropdownFormControl = new FormControl();
-  multipleDropdownFormControl = new FormControl();
-  manipulativeDropDownFormControl = new FormControl('one');
-  simpleAutocompleteFormControl = new FormControl();
-  featuredAutocompleteFormControl = new FormControl();
+
+  demoForm = new FormGroup({
+    simpleDropdown: this.simpleDropdownFormControl,
+    featuredDropdown: new FormControl(),
+    multipleDropdown: new FormControl(),
+    manipulativeDropDown: new FormControl('one'),
+    simpleAutocomplete: new FormControl(),
+    featuredAutocomplete: new FormControl(),
+  });
+
+  formSubmitValue = '';
 
   constructor() { }
 
   changeToThreeDemo(): void {
-    this.manipulativeDropDownFormControl.setValue('three');
+    this.demoForm.controls['manipulativeDropDown'].setValue('three');
+  }
+
+  onSubmit() {
+    this.formSubmitValue = this.demoForm.value;
   }
 }
