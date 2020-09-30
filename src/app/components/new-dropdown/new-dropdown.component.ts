a69327b33f59f3942fc39b0911660df2c6ddfc6d
nickg <nickg@email>
2020-09-30 10:41:29 +0300

    Recent for simple dropdown and form demo to grid

--- src/app/components/new-dropdown/new-dropdown.component.ts
+++ src/app/components/new-dropdown/new-dropdown.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 
@@ -7,14 +7,38 @@ import { FormControl } from '@angular/forms';
   templateUrl: './new-dropdown.component.html',
   styleUrls: ['./new-dropdown.component.scss']
 })
-export class NewDropdownComponent {
+export class NewDropdownComponent implements OnInit {
+
+  private readonly MAX_RECENT_LENGTH = 3;
+
+  @Input() control: FormControl;
 
   @Input() dropdownOptions: string[];
   @Input() type: 'dropdown' | 'multiple' | 'autocomplete' = 'dropdown';
   @Input() showIndex = false;
   @Input() showMultiple = false;
+  @Input() showRecent = false;
 
-  @Input() control: FormControl;
+  recent = [];
 
   constructor() { }
+
+  ngOnInit(): void {
+    if (this.showRecent) {
+      this.control.valueChanges.subscribe(value => this.newRecent(value));
+    }
+  }
+
+  private newRecent(value: string): void {
+    const valueIndex = this.recent.indexOf(value, 0);
+    if (valueIndex > -1) {
+      this.recent.splice(valueIndex, 1);
+    }
+
+    if (this.recent.length === this.MAX_RECENT_LENGTH) {
+      this.recent.shift();
+    }
+
+    this.recent.push(value);
+  }
 }
